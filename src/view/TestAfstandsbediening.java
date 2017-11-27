bb38bf362494f83121488454438fb9ae6df28bfa
Bart <bart@taelemans.org>
2017-11-27 20:00:37 +0100

    Aanmaken testclass

---
+++ src/view/TestAfstandsbediening.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package view;
+
+/**
+ * @author Bart Taelemans
+ * @date 27 nov. 2017
+ * @project Afstandsbediening
+ * @purpose Testclass om programma Afstandsbediening uit te voeren
+ *
+ */
+public class TestAfstandsbediening
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
